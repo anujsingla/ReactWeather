b089828eb7d26850efb1d7546a8887795ead9910
anujsingla <anujsingla.singla387@gmail.com>
2016-11-01 21:22:12 +0530

    example file edit

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 var Examples = React.createClass({
     render: function(){
         return(
+            <div>
             <h1>Examples compnent</h1>
+            <p> example paragraph</p>
+            </div>
+            
         );
     }
 });
